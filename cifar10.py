212297397b685842a8790ce6e3ea6bd38a1b47f9
Muhammad Irfan <m.irfan01@hotmail.com>
2021-06-26 22:11:35 -0700

    cifar10

--- cifar10.py
+++ cifar10.py
@@ -6,7 +6,7 @@ from torchvision import datasets, transforms
 import numpy as np
 from loss import contrastive_loss
 from models import Conv4
-from feature_bank import FeatureBank
+# from feature_bank import FeatureBank
 
 class CPCGridMaker:
     def __init__(self,grid_shape):
@@ -33,15 +33,15 @@ class CPCGridMaker:
 def main():
 
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('-bs','--batch-size', type=int, default=128, metavar='N',
+    parser.add_argument('-bs','--batch-size', type=int, default=256, metavar='N',
                         help='input batch size for training (default: 64)')
     parser.add_argument('-li','--logging-interval', type=int, default=100 ,
                         help='how often to print loss, every nth')
     parser.add_argument('-sm','--save-model', type=bool, default=True ,
                         help='should model be saved')
-    parser.add_argument('-e','--epochs', type=int, default=20 ,
+    parser.add_argument('-e','--epochs', type=int, default=200 ,
                         help='how many epochs to run')
-    parser.add_argument('-ns','--num_neg_samples', type=int, default=5 ,
+    parser.add_argument('-ns','--num_neg_samples', type=int, default=10 ,
                         help='how many negative samples to use')
     parser.add_argument('-wd',"--weight-decay",type=float,default=1e-5,
                         help=" weight decay for adam")
@@ -57,20 +57,18 @@ def main():
         transforms.Normalize((0.1307,), (0.3081,)),
         CPCGridMaker((8,8))
         ])
-    minist_train = datasets.CIFAR10('./data', train=True, download=True,
+    cifar_train = datasets.CIFAR10('./data', train=True, download=True,
                        transform=transform)
-    minist_test = datasets.CIFAR10('./data', train=False,
+    cifar_test = datasets.CIFAR10('./data', train=False,
                        transform=transform)
     grid_shape_x = 7
     K=2
     num_neg_sample = args.num_neg_samples
-    latent_size = 128
-    email_sara_mila_lo = False
+    latent_size = 512
+    email_sara_mila_lo = True
     
-
-
-    train_loader = torch.utils.data.DataLoader(minist_train,batch_size=args.batch_size)
-    test_loader = torch.utils.data.DataLoader(minist_test)
+    train_loader = torch.utils.data.DataLoader(cifar_train,batch_size=args.batch_size)
+    test_loader = torch.utils.data.DataLoader(cifar_test)
 
     model = Conv4(img_channels=3,K=K,latent_size=latent_size).to(device)
     model = model.double()
@@ -80,7 +78,7 @@ def main():
         model.train()
         total_loss = 0.0
         num_samples = 0
-        feature_bank = FeatureBank(max_len=10000)
+        # feature_bank = FeatureBank(max_len=10)
         for batch_idx,(data,_) in enumerate(train_loader):
             cur_batch = data.shape[0]
             data = data.to(device).double()
@@ -89,7 +87,7 @@ def main():
             grid_shape,img_shape = data.shape[:3],data.shape[3:]
             output = model(data.view(-1,*img_shape))
             output = output.view(*grid_shape,-1)
-            feature_bank.append(output.detach().cpu().numpy(),batch_idx)
+            # feature_bank.append(output.detach().cpu().numpy(),batch_idx)
             for c in range(grid_shape_x):
                 cl = output[:,:,c,:]
                 for t in range(grid_shape_x-K):
@@ -99,25 +97,23 @@ def main():
                     targets = cl[:,t+1:t+K+1,:]
                     for k in range(K):
                         pos_sample = targets[:,k,:] 
-
                         neg_idx = [i for i in range(grid_shape_x) if i!=c]
-                        feats = feature_bank.get_samples(num_neg_sample,batch_idx)
-                        ncl_data = np.stack(feats,axis=1)
+                        # feats = feature_bank.get_samples(num_neg_sample,batch_idx)
+                        # ncl_data = np.stack(feats,axis=1)
                         if email_sara_mila_lo:
-                            
-                            ncl = ncl_data[:,:,:,neg_idx,:]
-                            total_neg_sample = ncl.shape[1]*ncl.shape[2]*ncl.shape[3]
-                        else:
-                            ncl = ncl_data[:,:,k+t,neg_idx,:]
+                            ncl = output[:,:,neg_idx,:]
                             total_neg_sample = ncl.shape[1]*ncl.shape[2]
+                        else:
+                            ncl = output[:,k+t,neg_idx,:]
+                            total_neg_sample = ncl.shape[1]
                         neg_samples_arr = ncl.reshape((-1,total_neg_sample,latent_size))
 
                         neg_sample_idx = np.random.choice(total_neg_sample,num_neg_sample,replace=True)
-                        neg_samples = torch.from_numpy(neg_samples_arr[:cur_batch,neg_sample_idx,:]).to(device)
+                        neg_samples = neg_samples_arr[:cur_batch,neg_sample_idx,:]
                         loss += contrastive_loss(pos_sample,neg_samples,model.W[k],ar_out,norm=True)
             optimizer.zero_grad()
             loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 5)
+            # torch.nn.utils.clip_grad_norm_(model.parameters(), 5)
             optimizer.step()
             total_loss += loss.item()
             if batch_idx % args.logging_interval ==0:
@@ -129,12 +125,5 @@ def main():
                 "opt":optimizer.state_dict()
             },"cifar_epoch{}.pt".format(e))
                         
-
-    
-
-    
-    
-    
-
 if __name__ == '__main__':
     main()
--- models.py
+++ models.py
@@ -5,48 +5,45 @@ from torch.nn import functional as F
 from torch.nn.modules.linear import Linear
 import pytorch_lightning  as pl
 class Conv4(nn.Module):
-    def __init__(self,img_channels=1,hidden_size=100,K=2,latent_size=1024):
+    def __init__(self,img_channels=1,hidden_size=256,K=2,latent_size=512):
         super().__init__()
         self.latent_size = latent_size
-        self.feature = nn.Sequential(*[
-            nn.Conv2d(in_channels=img_channels, out_channels=32, kernel_size=3, stride=1,padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1,padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1,padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1,padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=32,out_channels=8, kernel_size=3, stride=1,padding=1),
-            nn.ReLU(),
-            nn.AdaptiveAvgPool2d(output_size=(4,4))
-        ])
-        
-        self.auto_regressive = nn.GRU(latent_size,hidden_size,1)
+        self.feature = nn.Sequential(nn.Conv2d(3, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5, inplace=False),
+            nn.Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5, inplace=False),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5, inplace=False),
+            nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5, inplace=False),
+            nn.Conv2d(128, 256, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5, inplace=False),
+            nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5, inplace=False),
+            nn.Conv2d(256, 512, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5, inplace=False),
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d(output_size=(1, 1)),
+        )
+        self.auto_regressive = nn.GRU(latent_size,hidden_size,2)
         self.W = nn.ModuleList([nn.Linear(hidden_size,latent_size,bias=False) for i in range(K)] )
-        
+        self.W = nn.ModuleList([nn.Sequential(nn.Linear(in_features=hidden_size, out_features=hidden_size//2, bias=True),
+        nn.Dropout(inplace=True),
+        nn.ReLU(inplace=True),
+        nn.Linear(in_features=hidden_size//2, out_features=latent_size, bias=True)) for i in range(K) ] )     
         self.K= K
-        # self.dropout1 = nn.Dropout(0.25)
-        # self.dropout2 = nn.Dropout(0.5)
-        # self.fc1 = nn.Linear(9216, 128)
-        # self.fc2 = nn.Linear(128, 10)
-
+        
     def forward(self, x):
         x = self.feature(x)
         return x
-        # x = F.max_pool2d(x, 2)
-        # x = self.dropout1(x)
-        # x = torch.flatten(x, 1)
-        # x = self.fc1(x)
-        # x = F.relu(x)
-        # # x = self.dropout2(x)
-        # x = self.fc2(x)
-        # output = F.log_softmax(x, dim=1)
-        # return output
+
 class LinClassifier(pl.LightningModule):
     def __init__(self,pretrain_path) -> None:
         super().__init__()
