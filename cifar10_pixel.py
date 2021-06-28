54598dca7498184e5455aaa81c1f71ad1c05431d
Muhammad Irfan <mirfan@ranctus.informatik.uni-freiburg.de>
2021-06-28 18:55:30 +0200

    changes

--- cifar10_pixel.py
+++ cifar10_pixel.py
@@ -5,9 +5,10 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 import numpy as np
 from loss import contrastive_loss
-from models import Conv4, Linear_Layer
+from models import Conv4
+# from feature_bank import FeatureBank
 import logging
-logging.basicConfig(filename='cifar10classifier.log', level=logging.DEBUG)
+logging.basicConfig(filename='pixelcnn.log', level=logging.DEBUG)
 class CPCGridMaker:
     def __init__(self,grid_shape):
         self.grid_shape = grid_shape
@@ -30,25 +31,29 @@ class CPCGridMaker:
                 else:
                     out[i,j,:,:,:] = sample[:,i*(grid_y//2):i*(grid_y//2)+grid_y,j*(grid_x//2):j*(grid_x//2)+grid_x]
         return out
-
 def main():
 
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
     parser.add_argument('-bs','--batch-size', type=int, default=64, metavar='N',
                         help='input batch size for training (default: 64)')
+    parser.add_argument('-li','--logging-interval', type=int, default=2 ,
+                        help='how often to print loss, every nth')
     parser.add_argument('-sm','--save-model', type=bool, default=True ,
                         help='should model be saved')
-    parser.add_argument('-e','--epochs', type=int, default=20 ,
+    parser.add_argument('-e','--epochs', type=int, default=200 ,
                         help='how many epochs to run')
-    parser.add_argument("-pt","--pretrain",type =str,help="path to the pretrain network")
-    parser.add_argument('-li','--logging-interval', type=int, default=10 ,
-                        help='how often to print loss, every nth')            
-    device = torch.device("cuda" if torch.cuda.is_available()  else "cpu")
+    parser.add_argument('-ns','--num_neg_samples', type=int, default=10 ,
+                        help='how many negative samples to use')
+    parser.add_argument('-wd',"--weight-decay",type=float,default=1e-5,
+                        help=" weight decay for adam")
+                           
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     args = parser.parse_args()
-    grid_shape_x = 7
-    K=2
-    latent_size = 512
-    email_sara_mila_lo = True
+    print(torch.cuda.is_available())
+    print(device)
+    # device = torch.device("cpu")
+    
+
     transform=transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,)),
@@ -58,49 +63,58 @@ def main():
                        transform=transform)
     cifar_test = datasets.CIFAR10('./data', train=False,
                        transform=transform)
-
-    train_loader = torch.utils.data.DataLoader(cifar_train,batch_size=args.batch_size,num_workers=4 )
+    grid_shape_x = 7
+    K=2
+    num_neg_sample = args.num_neg_samples
+    latent_size = 512
+    email_sara_mila_lo = True
+    
+    train_loader = torch.utils.data.DataLoader(cifar_train,batch_size=args.batch_size)
+    test_loader = torch.utils.data.DataLoader(cifar_test)
 
     model = Conv4(img_channels=3,K=K,latent_size=latent_size).to(device)
-    ckpt = torch.load("cifar_epoch15.pt")
-    model.load_state_dict(ckpt["model"])
-    logging.debug("Model loaded")
-    # if torch.cuda.is_available():
-    #     if torch.cuda.device_count() > 1:
-    #         print("works")
-    #         model = torch.nn.DataParallel(model)
-    # model = model.double()
-    optimizer = optim.Adam(model.parameters(), lr=0.1)
-    linear = Linear_Layer().to(device)
-    criterion = torch.nn.CrossEntropyLoss()
-
+    model = model.double()
+    optimizer = optim.Adam(model.parameters(),weight_decay=args.weight_decay)
+    
     for e in range(args.epochs):
-        model.eval()
-        linear.train()
+        model.train()
         total_loss = 0.0
         num_samples = 0
-        for batch_idx,(data,labels) in enumerate(train_loader):
+        for batch_idx,(data,_) in enumerate(train_loader):
+            cur_batch = data.shape[0]
+            data = data.to(device).double()
+            loss = torch.tensor(0.0).to(device)
             num_samples += data.shape[0]
-            data = data.to(device)
-            labels = labels.to(device)
             grid_shape,img_shape = data.shape[:3],data.shape[3:]
-            feature = model(data.view(-1,*img_shape))
-            feature = feature.view(grid_shape[0],-1)
-            output = linear(feature)
-            loss = criterion(output, labels)
+            output = model(data.view(-1,*img_shape))
+            output = output.view(*grid_shape,-1)
+            for t in range(grid_shape_x-K):
+                for c in range(grid_shape_x):        
+                    enc_grid = output[:,:t+1,:,:].view(cur_batch,-1,latent_size)
+                    enc_grid = enc_grid[:,:-(grid_shape_x-c-1) if (c <grid_shape_x-1) else grid_shape_x,:]
+                    ar_out,_ = model.auto_regressive(enc_grid)
+                    ar_out = ar_out[:,-1,:]
+                    targets = output[:,t+1:t+K+1,c,:]
+                    for k in range(K):
+                        pos_sample = targets[:,k,:] 
+                        neg_sample_idx = np.random.choice(grid_shape_x**2,num_neg_sample,replace=True)
+                        neg_samples = output.view(cur_batch,-1,latent_size)[:,neg_sample_idx,:]
+                        loss += contrastive_loss(pos_sample,neg_samples,model.W[k],ar_out,norm=True)
             optimizer.zero_grad()
             loss.backward()
+            # torch.nn.utils.clip_grad_norm_(model.parameters(), 5)
             optimizer.step()
             total_loss += loss.item()
             if batch_idx % args.logging_interval ==0:
                 print("average Loss is {:.4f}, batch_idx is {}/{}".format(loss.item()/data.shape[0],batch_idx,len(train_loader)))
-        
+                logging.debug("average Loss is {:.4f}, batch_idx is {}/{}".format(loss.item()/data.shape[0],batch_idx,len(train_loader)))
         print("Loss is {}, epoch is {}".format(total_loss/num_samples,e))
+        logging.debug("Loss is {}, epoch is {}".format(total_loss/num_samples,e))
         if args.save_model:
             torch.save({
                 "model":model.state_dict(),
                 "opt":optimizer.state_dict()
-            },"cifarclassifier_epoch{}.pt".format(e))
-
+            },"cifarpixel_epoch{}.pt".format(e))
+                        
 if __name__ == '__main__':
     main()
