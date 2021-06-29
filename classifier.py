83b22be2f4df0aceac52a36624e79ca1f781b45c
Muhammad Irfan <mirfan@ranctus.informatik.uni-freiburg.de>
2021-06-29 12:02:28 +0200

    lightning added

--- classifier.py
+++ classifier.py
@@ -5,7 +5,10 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 import numpy as np
 from loss import contrastive_loss
-from models import Conv4, Linear_Layer
+from pytorch_lightning import loggers as pl_loggers
+from models import Conv4, Linear_Layer, LinClassifier
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 import logging
 logging.basicConfig(filename='cifar10classifier.log', level=logging.DEBUG)
 class CPCGridMaker:
@@ -38,7 +41,7 @@ def main():
                         help='input batch size for training (default: 64)')
     parser.add_argument('-sm','--save-model', type=bool, default=True ,
                         help='should model be saved')
-    parser.add_argument('-e','--epochs', type=int, default=20 ,
+    parser.add_argument('-e','--epochs', type=int, default=500 ,
                         help='how many epochs to run')
     parser.add_argument("-pt","--pretrain",type =str,help="path to the pretrain network")
     parser.add_argument('-li','--logging-interval', type=int, default=10 ,
@@ -46,8 +49,6 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available()  else "cpu")
     args = parser.parse_args()
     grid_shape_x = 7
-    K=2
-    latent_size = 512
     email_sara_mila_lo = True
     transform=transforms.Compose([
         transforms.ToTensor(),
@@ -61,63 +62,25 @@ def main():
 
     train_loader = torch.utils.data.DataLoader(cifar_train,batch_size=args.batch_size,num_workers=4 )
     test_loader = torch.utils.data.DataLoader(cifar_test,batch_size=args.batch_size,num_workers=4 )
-    model = Conv4(img_channels=3,K=K,latent_size=latent_size).to(device)
-    ckpt = torch.load("cifar_epoch15.pt")
-    model.load_state_dict(ckpt["model"])
-    logging.debug("Model loaded")
-    # if torch.cuda.is_available():
-    #     if torch.cuda.device_count() > 1:
-    #         print("works")
-    #         model = torch.nn.DataParallel(model)
-    # model = model.double()
-    optimizer = optim.Adam(model.parameters(), lr=0.1)
-    linear = Linear_Layer().to(device)
-    criterion = torch.nn.CrossEntropyLoss()
-
-    for e in range(args.epochs):
-        model.eval()
-        linear.train()
-        total_loss = 0.0
-        num_samples = 0
-        for batch_idx,(data,labels) in enumerate(train_loader):
-            num_samples += data.shape[0]
-            data = data.to(device)
-            labels = labels.to(device)
-            grid_shape,img_shape = data.shape[:3],data.shape[3:]
-            feature = model(data.view(-1,*img_shape))
-            feature = feature.view(grid_shape[0],-1)
-            output = linear(feature)
-            loss = criterion(output, labels)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            total_loss += loss.item()
-            if batch_idx % args.logging_interval ==0:
-                print("average Loss is {:.4f}, batch_idx is {}/{}".format(loss.item()/data.shape[0],batch_idx,len(train_loader)))
-                logging.debug("average Loss is {:.4f}, batch_idx is {}/{}".format(loss.item()/data.shape[0],batch_idx,len(train_loader)))
-        print("Loss is {}, epoch is {}".format(total_loss/num_samples,e))
-        logging.debug("Loss is {}, epoch is {}".format(total_loss/num_samples,e))
-        model.eval()
-        linear.eval()
-        eval_loss = 0.0
-        eval_total = 0.0
-        for batch_idx,(data,labels) in enumerate(test_loader):
-            eval_total += data.shape[0]
-            data = data.to(device)
-            labels = labels.to(device)
-            grid_shape,img_shape = data.shape[:3],data.shape[3:]
-            feature = model(data.view(-1,*img_shape))
-            feature = feature.view(grid_shape[0],-1)
-            output = linear(feature)
-            loss = criterion(output, labels)
-            eval_loss += loss.item()
-        print("Eval Loss is {}, epoch is {}".format(total_loss/eval_total,e))
-        logging.debug("Eval Loss is {}, epoch is {}".format(total_loss/eval_total,e))
-        if args.save_model:
-            torch.save({
-                "model":model.state_dict(),
-                "opt":optimizer.state_dict()
-            },"cifarclassifier_epoch{}.pt".format(e))
+    model = LinClassifier("/project/dl2021s/mirfan/cifar_epoch21.pt")
+    from pytorch_lightning.callbacks import ModelCheckpoint
+    early_stop_callback = EarlyStopping(
+        monitor='val_accuracy',
+        min_delta=0.0,
+        patience=50,
+        verbose=True,
+        mode='max'
+        )
+    tb_logger = pl_loggers.TensorBoardLogger('logs/',name="cifar10runs")
+    checkpoint_callback = ModelCheckpoint(
+    monitor='val_accuracy',
+    dirpath='logs/cifar10runs',
+    filename='cifar-{epoch:02d}-{val_accuracy:.2f}',
+    save_top_k=3,
+    mode='min',
+)
+    trainer = pl.Trainer(gpus=1,max_epochs= args.epochs,min_epochs=1,logger=tb_logger, callbacks=[early_stop_callback,checkpoint_callback])
+    trainer.fit(model, train_loader, test_loader)
 
 if __name__ == '__main__':
     main()
--- models.py
+++ models.py
@@ -1,9 +1,9 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
-
+import pytorch_lightning  as pl
 from torch.nn.modules.linear import Linear
-
+from pytorch_lightning.utilities.types import EPOCH_OUTPUT
 class Conv4(nn.Module):
     def __init__(self,img_channels=1,hidden_size=256,K=2,latent_size=512):
         super().__init__()
@@ -33,7 +33,7 @@ class Conv4(nn.Module):
             nn.AdaptiveAvgPool2d(output_size=(1, 1)),
         )
         self.auto_regressive = nn.GRU(latent_size,hidden_size,2)
-        self.W = nn.ModuleList([nn.Linear(hidden_size,latent_size,bias=False) for i in range(K)] )
+        # self.W = nn.ModuleList([nn.Linear(hidden_size,latent_size,bias=False) for i in range(K)] )
         self.W = nn.ModuleList([nn.Sequential(nn.Linear(in_features=hidden_size, out_features=hidden_size//2, bias=True),
         nn.Dropout(),
         nn.ReLU(),
@@ -58,3 +58,70 @@ class Linear_Layer(nn.Module):
     def forward(self, x):
         x = self.feature(x)
         return x
+
+class LinClassifier(pl.LightningModule):
+    def __init__(self,pretrain_path) -> None:
+        super().__init__()
+        K=2
+        latent_size = 512
+        self.feat = Conv4(img_channels=3)
+        ckpt = torch.load(pretrain_path)
+        self.feat.load_state_dict(ckpt["model"])
+        for param in self.feat.parameters():
+            param.requires_grad=False
+        
+        if hasattr(self.feat,"latent_size"):
+            self.latent_size = self.feat.latent_size
+        else:
+            self.latent_size = 512
+
+        self.lin = nn.Sequential(nn.Linear(25088, 1024, bias=True),
+        nn.Dropout(),
+        nn.ReLU(inplace=True),
+        nn.Linear(1024, 512, bias=True),
+        nn.Dropout(),
+        nn.ReLU(inplace=True),
+        nn.Linear(512, 10, bias=True))
+        self.val_acc = 0
+    
+    def forward(self,x):
+        # x = torch.unsqueeze(x,-3)
+        batch_grid, img_shape = x.shape[:3],x.shape[3:]
+        res = self.feat.feature(x.view(-1,*img_shape))
+        res = res.view(batch_grid[0],-1)
+        x = self.lin(res)
+        return x
+
+    def cross_entropy_loss(self,logits,labels):
+        return torch.nn.CrossEntropyLoss()(logits,labels)
+    def training_step(self,train_batch,batch_idx):
+        data,target = train_batch
+        logits = self.forward(data)
+        loss = self.cross_entropy_loss(logits,target)
+        accuracy = self.accuracy(logits, target)
+        self.log("train_accuracy", accuracy)
+        self.log("train_loss",loss)
+        return loss
+    def validation_step(self,val_batch,batch_idx):
+        data, target = val_batch
+        logits = self.forward(data)
+        loss = self.cross_entropy_loss(logits,target)
+        accuracy = self.accuracy(logits, target)
+        self.log("val_loss",loss)
+        self.log("val_accuracy",accuracy)
+        
+        return {"val_loss": loss, "val_accuracy": accuracy}
+    def validation_epoch_end(self, outputs: EPOCH_OUTPUT) -> None:
+        
+        accuracy = torch.tensor([x["val_accuracy"] for x in outputs]).mean()
+        if self.val_acc <accuracy:
+            self.val_acc = accuracy
+        return super().validation_epoch_end(outputs)
+    def configure_optimizers(self):
+        opt = torch.optim.Adam(self.parameters(),lr=1e-3)
+        return opt
+    def accuracy(self, logits, labels):
+        _, predicted = torch.max(logits.data, 1)
+        correct = (predicted == labels).sum().item()
+        accuracy = correct / len(labels)
+        return torch.tensor(accuracy)
