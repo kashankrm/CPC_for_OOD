d5892836afcd1e21f2812324ba965daee20bd31c
Kashan Karim <mkashankarim@gmail.com>
2021-07-23 19:19:37 +0200

    added softmax to loss

--- loss.py
+++ loss.py
@@ -1,4 +1,5 @@
 import torch
+from torch.nn import functional as F
 
 def contrastive_loss(positive, negatives, W, context, temp = 0.5,norm=True,indivisual_loss =False):
     if len(positive.shape)!=3:
@@ -18,12 +19,16 @@ def contrastive_loss(positive, negatives, W, context, temp = 0.5,norm=True,indiv
         pos_fk = pos_fk/((torch.norm(positive,dim=(1,2)) * torch.norm(c_w,dim=1))+torch.tensor(1e-6))
         neg_fk = neg_fk/((torch.norm(negatives,dim=2)* torch.norm(c_w,dim=1).unsqueeze(1))+torch.tensor(1e-6))
 
-    pos_fk = torch.exp(pos_fk)
-    neg_fk = torch.exp(neg_fk)
-    denom = torch.sum(neg_fk,dim=1)+pos_fk
-    loss = torch.log(pos_fk/denom)
-    loss += torch.sum(torch.log(neg_fk/denom.unsqueeze(1)),dim=1)       
+    pos_fk = pos_fk.unsqueeze(dim=1)
+    loss = torch.cat([pos_fk,neg_fk],dim=1)
+    loss = F.softmax(loss,dim=1)
+    loss = -loss[:,0]
+    # pos_fk = torch.exp(pos_fk)
+    # neg_fk = torch.exp(neg_fk)
+    # denom = torch.sum(neg_fk,dim=1)+pos_fk
+    # loss = torch.log(pos_fk/denom)
+    # loss += torch.sum(torch.log(neg_fk/denom.unsqueeze(1)),dim=1)       
     if indivisual_loss:
-        return -(1/(neg_fk.shape[1]+1))*loss
+        return loss
     else:
-        return -(1/(neg_fk.shape[1]+1))*loss.sum()
+        return loss.sum()
